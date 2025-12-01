d854e58ea332cc44e4fc3bc3c79104dc20d8f3d0
Yousef Mohamed <egytech45189@gmail.com>
2025-12-01 07:24:26 +0200

    Remove unused short_description association from ShortDescriptable concern to enhance clarity and maintainability of the model.

--- app/models/concerns/short_descriptable.rb
+++ app/models/concerns/short_descriptable.rb
@@ -6,15 +6,12 @@ module ShortDescriptable
   included do
     has_many :short_descriptions, as: :resource
 
-    # For eager loading (not currently used, but mirrors NameTranslateable pattern)
-    has_one :short_description, as: :resource
   end
 
   def short_description_for_language(language_code)
     language = Language.find_with_id_or_iso_code(language_code)
     return nil unless language
 
-    # Try requested language first, then default
     short_descriptions.find { |sd| sd.language_id == language.id } ||
       short_descriptions.find { |sd| sd.language_id == Language.default.id }
   end
